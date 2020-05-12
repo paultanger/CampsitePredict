b7b10013b02d30e45a7bf22c24f95a4d78fd9c3c
Paul Tanger <detroitstylz@gmail.com>
2020-05-11 20:56:07 -0600

    map attempt 1

--- app.py
+++ app.py
@@ -8,8 +8,13 @@ import json
 from bokeh.models import GeoJSONDataSource 
 from bokeh.plotting import figure, curdoc
 from bokeh.layouts import column
+from bokeh.io import output_file, show
+from bokeh.models import ColumnDataSource, GMapOptions
+from bokeh.plotting import gmap
 
 app = Flask(__name__)
+
+
 
 # https://pjandir.github.io/Bokeh-Heroku-Tutorial/
 
@@ -17,9 +22,9 @@ app = Flask(__name__)
 # shapefile = 'data/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp'
 # gdf = gpd.read_file(shapefile)[['ADMIN', 'ADM0_A3', 'geometry']]
 
-# Rename the columns
+# # Rename the columns
 # gdf.columns = ['country', 'country_code', 'geometry']
-# Convert the GeoDataFrame to GeoJSON format so it can be read by Bokeh
+# # Convert the GeoDataFrame to GeoJSON format so it can be read by Bokeh
 # merged_json = json.loads(gdf.to_json())
 # json_data = json.dumps(merged_json)
 # geosource = GeoJSONDataSource(geojson=json_data)
@@ -47,13 +52,34 @@ def index():
 @app.route('/home')
 def home():
     
+    output_file("gmap.html")
+
+    map_options = GMapOptions(lat=30.2861, lng=-97.7394, map_type="roadmap", zoom=11)
+    
+    p = gmap("AIzaSyB9IAkbG2YcspA3G1PfxWl5CcmLfSEyr9Q", map_options, title="Austin")
+    
+    source = ColumnDataSource(
+        data=dict(lat=[ 30.29,  30.20,  30.29],
+                  lon=[-97.70, -97.74, -97.78])
+    )
+    
+    p.circle(x="lon", y="lat", size=15, fill_color="blue", fill_alpha=0.8, source=source)
+    
+    # for testing
+    #show(p)
+    
+    # for flask / heroku
+    curdoc().add_root(column(p))
+
     # plot = figure(tools=TOOLS,
     #           title='Data from Quandle WIKI set',
     #           x_axis_label='date',
     #           x_axis_type='datetime')
     
     # script, div = components(plot)
-    return render_template('home.html', title='home of maps')
+    
+    #return render_template('home.html', title='home of maps')
+    
     #return render_template('home.html', script=script, div=div)
 
 if __name__ == '__main__':
