cc7220a43bbe435ab274448fb71f36821ccbed17
Paul Tanger <detroitstylz@gmail.com>
2020-05-11 19:47:07 -0600

    update home.html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from bokeh.embed import components 
 from datetime import datetime
 app = Flask(__name__)
     
@@ -10,7 +11,15 @@ def index():
 # subpage
 @app.route('/home')
 def home():
-    return render_template('home.html', title='home of maps')
+    
+    # plot = figure(tools=TOOLS,
+    #           title='Data from Quandle WIKI set',
+    #           x_axis_label='date',
+    #           x_axis_type='datetime')
+    
+    # script, div = components(plot)
+    #return render_template('home.html', title='home of maps')
+    return render_template('home.html', script=script, div=div)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
