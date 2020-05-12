cf315d604489248a3422ac76492d302d3009a47d
Paul Tanger <detroitstylz@gmail.com>
2020-05-11 18:18:59 -0600

    update app

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
-
+    
+# the main (so like index)
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+def index():
+    return render_template('index.html', title='main')
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+# subpage
+@app.route('/home')
+def home():
+    return render_template('home.html', title='home of maps')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
