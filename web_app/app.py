f74d4e12d4375b99bf5bea65428e0dce1487b5a8
Paul Tanger <detroitstylz@gmail.com>
2020-09-21 22:00:22 -0600

    web text edits

--- web_app/app.py
+++ web_app/app.py
@@ -101,6 +101,10 @@ def get_file(filename):
 def home():
     return render_template("index.html")
 
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template("about.html")
+
 # display sample from df for testing
 @app.route('/analysis') 
 def analysis():
